ec32340c686d60bceb9f1e217817c829056457d2
Mark Ryder <markmryder@gmail.com>
2021-01-11 10:56:45 -0500

    Initial commit

---
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+public class RTSNetworkManager : NetworkManager
+{
+	[SerializeField] private GameObject unitSpawnerPrefab = null;
+
+	public override void OnServerAddPlayer(NetworkConnection conn)
+	{
+		base.OnServerAddPlayer(conn);
+
+		GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+		NetworkServer.Spawn(unitSpawnerInstance, conn);
+	}
+}
---
+++ Assets/Scripts/Player/UnitMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class UnitMovement : NetworkBehaviour
+{
+
+	[SerializeField] private NavMeshAgent agent = null;
+	private Camera mainCamera;
+
+	#region Server
+
+	[Command]
+	private void CmdMove(Vector3 position)
+	{
+		if(!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas))
+		{
+			return;
+		}
+		agent.SetDestination(hit.position);
+	}
+
+
+	#endregion
+
+	#region Client
+
+	public override void OnStartAuthority()
+	{
+		mainCamera = Camera.main;
+		//base.OnStartAuthority();
+	}
+
+	[ClientCallback]
+
+	private void Update()
+	{
+		if (!hasAuthority)
+		{
+			return;
+		}
+		if (!Mouse.current.rightButton.wasPressedThisFrame)
+		{
+			return;
+		}
+
+		Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+		if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+		{
+			return;
+		}
+		CmdMove(hit.point);
+	}
+
+	#endregion
+}
